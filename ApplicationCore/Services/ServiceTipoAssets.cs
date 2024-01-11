f2961698e802a58cc4f41cea7f65018825f63546
geraldSinertica <139481630+geraldSinertica@users.noreply.github.com>
2024-01-11 12:11:26 -0600

    add type asset

---
+++ ApplicationCore/Services/ServiceTipoAssets.cs
@@ -0,0 +1,23 @@
+﻿using Infraestructure.Models;
+using Infraestructure.Repositorys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class ServiceTipoAssets : IServiceTipoAssets
+    {
+        private IRepositoryTipoAssets repository;
+        public ServiceTipoAssets()
+        {
+            repository = new RepositoryTipoAssets();
+        }
+        public async Task<IEnumerable<TipoAssets>> getAll()
+        {
+            return await repository.getAll();  
+        }
+    }
+}
---
+++ PimasUsuario/Controllers/Api/TipoAssetsApiController.cs
@@ -0,0 +1,54 @@
+﻿using ApplicationCore.Services;
+using Infraestructure.Models;
+using PimasUsuario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PimasUsuario.Controllers.Api
+{
+    [RoutePrefix("api/TypeAssets")]
+    public class TipoAssetsApiController : ApiController
+    {
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IHttpActionResult> getByTematic(int tematicId, int group)
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                IServiceTipoAssets service = new ServiceTipoAssets();
+
+                IEnumerable<TipoAssets> types = await service.getAll();
+
+                if (types == null || !types.Any())
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    response.Message = "Tipos no encontrados ";
+
+                }
+                else
+                {
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    response.Message = "Datos encontrados";
+                    response.Data = types;
+                }
+
+                return Json(response);
+            }
+            catch (Exception e)
+            {
+
+                response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                response.Message = e.Message;
+
+                return Json(response);
+            }
+        }
+    }
+}
